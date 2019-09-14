0497a3c4314947fd8582fcb7244a3f1a3a1433cd
Justin Carlson <justincarlson7@gmail.com>
2019-09-14 08:54:01 -0400

    trying to make it work  part 2 - added all the questions, updated the comments and fleshed out the pseudo code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,90 +1,78 @@
-
-
-
-
 $(document).ready(function() {
+  var timer = 10;
+  var intervalID = 0;
+  var correctAnswers = 0;
+  var incorrectAnswers = 0;
 
+  // $("input[type='button']").click(function(){
+  //   var radioValue = $("input[name='questionOne']:checked").val();
+  //   if(radioValue = "option1"){
+  //       console.log("radioValue", radioValue);
+  //   }
+  // });
 
-  
-var timer = 10;
-var intervalID = 0;
-var correctAnswers = 0;
-var incorrectAnswers = 0;
+  //Set interval ID funtion
 
+  function run() {
+    intervalID = setInterval(decrement, 1000);
+  }
 
-// $("input[type='button']").click(function(){
-//   var radioValue = $("input[name='questionOne']:checked").val();
-//   if(radioValue = "option1"){
-//       console.log("radioValue", radioValue);
-//   }
-// });
+  //Start count down timer function
 
+  function decrement() {
+    timer--;
 
+    console.log("time " + timer);
 
+    $("#timerNums").html("<h2> Timer: " + timer + "</h2>");
 
+    //stop time when it gets to 0
 
+    if (timer === 0) {
+      alert("Time up!");
+      clearInterval(intervalID);
+      // timer = 10;
+    }
+  }
 
+  //Reset timer with set interval function
 
-function run(){
+  run();
 
-intervalID = setInterval(decrement, 1000);
+  //function to capture onclick as correct or incorrect - CAN'T MAKE IT WORK !!!!!!
 
+  $("input[type='radio']").click(function() {
+    var radioValue = $("input[name='riggs']:checked").val();
 
-}
+    if ((radioValue = "true")) {
+      console.log("You got it correct!" + radioValue);
+    } else {
+      console.log("Incorrect - ");
+    }
+  });
 
+  //PSEUDO CODE
 
-function decrement() {
+  //use .hide to have start button and reload to quick
+  // onclick correct answer
+  // increment correct incorrect variables
+  // display counters at increment
+  //Display TIMES UP as part of conditional statement related to timer
+  //Include a restart game button on Times up code
 
-timer--;
+  //Additional code used for testing
 
-console.log("time " + timer);
-
-$("#timerNums").html("<h2> Timer: " + timer + "</h2>");
-
-if (timer === 0) {
-
-    alert("Time up!");
-    clearInterval(intervalID);
-    // timer = 10;
-}
-}
-
-run();
-
-$("input[type='radio']").click(function(){
-  var radioValue = $("input[name='riggs']:checked").val();
-
-  
-  if (radioValue = "true") {
-
-  console.log("You got it correct!" + radioValue)
-
-} else { console.log("WRONG!!!!")
-
-}
-
-});
-
-// onclick answer
-// if class is true
-// show moreInfo
-// Else show wrong anser
-
-
-// $('.Question1').on('click',function() {
-//     alert($(this).val());
-//     console.log($(this).val());
-//   });
-    
+  // $('.Question1').on('click',function() {
+  //     alert($(this).val());
+  //     console.log($(this).val());
+  //   });
 
   //Start game page - instructions and button
-//   $("#hide").click(function(){
-//     $("p").hide();
-//   });
-  
-//   $("#show").click(function(){
-//     $("p").show();
-//   });
+  //   $("#hide").click(function(){
+  //     $("p").hide();
+  //   });
 
-
-})
+  //   $("#show").click(function(){
+  //     $("p").show();
+  //   });
+});
