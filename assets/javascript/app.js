f68a14c0b087ec0ca23913d1e00f7eb8080326b3
Justin Carlson <justincarlson7@gmail.com>
2019-09-15 17:24:38 -0400

    completed the logic for the game, including the hide code for start and summary pages.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,25 @@
+  //PSEUDO CODE
+
+  //use .hide to have start button and reload to quick
+  // onclick correct answer
+  // increment correct incorrect variables
+  // display counters at increment
+  //Display TIMES UP as part of conditional statement related to timer
+  //Include a restart game button on Times up code
+
+
 $(document).ready(function() {
-  var timer = 10;
-  var intervalID = 0;
-  var correctAnswers = 0;
-  var incorrectAnswers = 0;
+
+  $('#countDown').hide();
+  $('.questions').hide();
+  $('.results').hide();
+
+
+  var timerCounter = 30;
+  var intervalId;
+  var correctItems = 0;
+  var wrongItems = 0;
+  var unanswered = 0;
 
   // $("input[type='button']").click(function(){
   //   var radioValue = $("input[name='questionOne']:checked").val();
@@ -13,52 +30,89 @@ $(document).ready(function() {
 
   //Set interval ID funtion
 
-  function run() {
-    intervalID = setInterval(decrement, 1000);
+  function showQuestions(){
+    $('#countDown').show();
+    $('.questions').show();
+    $('#game-done').show();
+}
+
+
+function countDownTimer(){
+  intervalId = setInterval(decrement, 1000);
+}
+
+function decrement(){
+  timerCounter--;
+  $('#timer').html(" " + timerCounter + " " + "seconds");
+  if (timerCounter ===1){
+      $('#timer').html(" " + timerCounter + " " + "second");
   }
-
-  //Start count down timer function
-
-  function decrement() {
-    timer--;
-
-    console.log("time " + timer);
-
-    $("#timerNums").html("<h2> Timer: " + timer + "</h2>");
-
-    //stop time when it gets to 0
-
-    if (timer === 0) {
-      alert("Time up!");
-      clearInterval(intervalID);
-      // timer = 10;
-    }
+  else if(timerCounter ===0) {
+      stop();
+      hide();
+      gameSummary();
   }
+}
 
-  //Reset timer with set interval function
+  //stop timer function
 
-  run();
+  function stop() {
+    clearInterval(intervalId);
+}
 
-  //function to capture onclick as correct or incorrect - CAN'T MAKE IT WORK !!!!!!
+//Hide functions
+function hide(){
+  $('#countDown').hide();
+  $('.questions').hide();
+  $('#game-done').hide();
+}
 
-  $("input[type='radio']").click(function() {
-    var radioValue = $("input[name='riggs']:checked").val();
+//game stats
 
-    if ((radioValue = "true")) {
-      console.log("You got it correct!" + radioValue);
-    } else {
-      console.log("Incorrect - ");
-    }
+
+function gameSummary(){
+  $('.results').show();
+  unanswered = (8-(correctItems+wrongItems));
+  $('#correctScore').text("Correct Answers:" + " " + correctItems); 
+  $('#wrongScore').text("Wrong Answers:" + " " + wrongItems); 
+  $('#unanswered').text("Unanswered:" + " " + unanswered); 
+}
+
+
+$('.game-start').on('click', function(){
+  $('.game-start').hide();
+  showQuestions();
+  countDownTimer();
+}); 
+
+$('#game-done').on('click', function(){
+  $('.game-start').hide(); 
+  hide();
+  gameSummary();
+});
+
+$('input[type=radio]').on ('change', function(){
+  correctItems = $('input[value=correct]:checked').length;
+  wrongItems = $('input[value=wrong]:checked').length;
+  unanswered = (8-(correctItems+wrongItems));
   });
 
-  //PSEUDO CODE
 
-  //use .hide to have start button and reload to quick
-  // onclick correct answer
-  // increment correct incorrect variables
-  // display counters at increment
-  //Display TIMES UP as part of conditional statement related to timer
-  //Include a restart game button on Times up code
+});
+
+//     radioValueArray = radioValueArray.push(radioValue)
+
+// console.log(answers[i]);
+
+    //if ((radioValue === "correct")) {
+    //   console.log("You got it correct! " + radioValue);
+      
+    // } else {
+    //   console.log("Incorrect - ", radioValue);
+    // }
+
+
+
 
   //Additional code used for testing
 
@@ -75,4 +129,4 @@ $(document).ready(function() {
   //   $("#show").click(function(){
   //     $("p").show();
   //   });
-});
+
